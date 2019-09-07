f40ee87e72142114120bd9caf470ded1181b5e16
Lokesh Khandelwal <42270097+lokeshkh92@users.noreply.github.com>
2019-09-07 13:07:50 +0530

    Add files via upload

---
+++ file_edit.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import gzip
+import glob
+import shutil
+
+ZIPFILES='/Users/lokeshkhandelwal/Downloads/master*.gz'
+
+#Get file list from directory
+filelist = glob.glob(ZIPFILES)
+
+for gzfile in filelist:
+
+    res = []
+    with gzip.open(gzfile, 'rt') as fr:
+        for line in fr:
+            #print(line)
+            lines = line.split(',')
+            RecordTime = lines[0]
+            rmngPartnerNWId = lines[22]
+            partnerNWId = lines[23]
+            #print("RecordTime: {}".format(RecordTime))
+            #print("Original rmngPartner: {}".format(rmngPartnerNWId))
+            #print("Original Partner: {}".format(partnerNWId))
+            #Copy value of partnerNWId to rmngPartnerNWId
+            if lines[2] == '3' and lines[22] == '0':
+                lines[22] = lines[23]
+            #print("New rmngPartner: {}".format(lines[22]))
+            #print(lines)
+            res.append(','.join(lines))
+
+    with gzip.open(gzfile, 'wt') as fw:
+        for modified_lines in res:
+            fw.write(modified_lines)
+
+    #Move file to another location
+    shutil.move(gzfile, '/Users/lokeshkhandelwal')
